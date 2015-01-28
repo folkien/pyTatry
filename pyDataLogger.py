e246271fce78f264b56d8703dd340e45b1ab6237
folkien <folkus@gmail.com>
2015-01-28 16:00:49 +0100

    Dodano parsowanie XML oraz prostą bazę danych XML.

--- pyDataLogger.py
+++ pyDataLogger.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import urllib2, datetime
+import urllib2, datetime, sqlite3, os
 
-photosFolder = "./data/photos/"
-
+databaseFile = os.getenv("HOME") + "/python/pyTatry/data/database.db"
 
 def saveFile(filename,data):
 		f = open(filename,'w')
@@ -18,16 +17,24 @@ def createFileName(sufix):
 					format(now.hour,'02') + " " + \
 					format(now.minute,'02') + " " + sufix
 
-# Temperatury pobieramy stąd
-# 
+
+# Łączymy się z bazą danych.
+conn = sqlite3.connect('example.db')
+#Tworzymy tabele jeżeli nie istnieje
+conn.execute("create table if not exists data ( id integer, localization text, \
+												moment DATETIMe, desc text,    \
+												value real)")
+conn.commit()
 
 
 from xml.dom import minidom
 response = urllib2.urlopen('http://www.test.tatrynet.pl/pogoda/weatherMiddleware_v1.0/xml/lokalizacje1.xml')
 xmldoc = minidom.parseString(response.read())
-print xmldoc
-#itemlist = xmldoc.getElementsByTagName('item') 
-#print len(itemlist)
-#print itemlist[0].attributes['name'].value
-#for s in itemlist :
-#    print s.attributes['name'].value
+#print xmldoc
+for node in xmldoc.getElementsByTagName('lokalizacja'):  # visit every node
+	print node.getAttribute("id")
+	print node.getElementsByTagName("temperatura")[0].getElementsByTagName("aktualna")[0].childNodes[0].toxml()
+
+#Zapis danych i zamknięcie bazydanych.
+conn.commit()
+conn.close()

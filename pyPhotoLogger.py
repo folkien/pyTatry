fb52255cfa2862c4c79b324d684a9deaca6f8a12
folkien <folkus@gmail.com>
2015-01-28 15:18:00 +0100

    Poprawki położenia katalogów oraz wyświetlanie akutalnej operacji.

--- pyPhotoLogger.py
+++ pyPhotoLogger.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import urllib2, datetime
+import urllib2, datetime, os
 
-photosFolder = "./data/photos/"
-
+photosFolder = os.getenv("HOME") + "/python/pyTatry/data/photos/"
 
 def saveFile(filename,data):
 		f = open(filename,'w')
@@ -19,35 +18,35 @@ def createFileName(sufix):
 					format(now.minute,'02') + sufix
 
 
-#Moko 1
+print "Morskie oko 1."
 response = urllib2.urlopen('http://kamery.topr.pl/moko/moko_01.jpg')
 saveFile(photosFolder + "moko1/" + createFileName('.jpg'), response.read())
 
-#Moko 2
+print "Morskie oko 2."
 response = urllib2.urlopen('http://kamery.topr.pl/moko_TPN/moko_02.jpg')
 saveFile(photosFolder + "moko2/" +createFileName('.jpg'), response.read())
 
-#5 stawów
+print "Dolina 5 stawów."
 response = urllib2.urlopen('http://kamery.topr.pl/stawy2/stawy2.jpg')
 saveFile(photosFolder + "5stawow/" +createFileName('.jpg'), response.read())
 
-#buczynowa
+print "Buczynowa dolinka."
 response = urllib2.urlopen('http://kamery.topr.pl/stawy1/stawy1.jpg')
 saveFile(photosFolder + "buczynowa/" +createFileName('.jpg'), response.read())
 
-#gasienicowa
+print "Gąsienicowa dolina."
 response = urllib2.urlopen('http://kamery.topr.pl/gasienicowa/gasie.jpg')
 saveFile(photosFolder + "gasienicowa/" +createFileName('.jpg'), response.read())
 
-#goryczkowa
+print "Goryczkowa dolina."
 response = urllib2.urlopen('http://kamery.topr.pl/goryczkowa/gorycz.jpg')
 saveFile(photosFolder + "goryczkowa/" +createFileName('.jpg'), response.read())
 
-#chocholowska
+print "Chochołowska dolina."
 response = urllib2.urlopen('http://kamery.topr.pl/chocholowska/chocholow.jpg')
 saveFile(photosFolder + "chocholowska/" +createFileName('.jpg'), response.read())
 
-#Meteogram
+print "Meteogram."
 response = urllib2.urlopen('http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=2015012806&row=487&col=232&lang=pl')
 saveFile(photosFolder + "meteo/" +createFileName('.png'), response.read())
 

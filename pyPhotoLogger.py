6dea3202ddf6b2c1aeb6d4268740be5227a4a9a9
spasz <folkus@gmail.com>
2015-02-01 11:48:42 +0100

    Dodano mergowanie zdjęc w jedno zawierające obraz ze wszystkich kamer, dzięki czemu można podejrzeć cały dzień w tatrach.

--- pyPhotoLogger.py
+++ pyPhotoLogger.py
@@ -11,43 +11,61 @@ def saveFile(filename,data):
 
 def createFileName(sufix):
 		now = datetime.datetime.now()
-		return format(now.year,'02') + "." + \
-					format(now.month,'02') + "." + \
-					format(now.day,'02') + "_" + \
-					format(now.hour,'02') + ":" + \
-					format(now.minute,'02') + sufix
+		return  format(now.year,'02') + "." + \
+                        format(now.month,'02') + "." + \
+                        format(now.day,'02') + "_" + \
+                        format(now.hour,'02') +  \
+                        format(now.minute,'02') + sufix
+
+# Tworzymy nazwe pliku zgodnie z aktualna data
+fileName = createFileName('.jpg')
+m1FileName = photosFolder + "moko1/" + fileName
+m2FileName = photosFolder + "moko2/" + fileName
+psFileName = photosFolder + "5stawow/" + fileName
+buFileName = photosFolder + "buczynowa/" + fileName
+gaFileName = photosFolder + "gasienicowa/" + fileName
+goFileName = photosFolder + "goryczkowa/" + fileName
+chFileName = photosFolder + "chocholowska/" + fileName
+allFileName= photosFolder + "tatry/" + fileName
 
 
 print "Morskie oko 1."
 response = urllib2.urlopen('http://kamery.topr.pl/moko/moko_01.jpg')
-saveFile(photosFolder + "moko1/" + createFileName('.jpg'), response.read())
+saveFile(m1FileName, response.read())
 
 print "Morskie oko 2."
 response = urllib2.urlopen('http://kamery.topr.pl/moko_TPN/moko_02.jpg')
-saveFile(photosFolder + "moko2/" +createFileName('.jpg'), response.read())
+saveFile(m2FileName, response.read())
 
 print "Dolina 5 stawów."
 response = urllib2.urlopen('http://kamery.topr.pl/stawy2/stawy2.jpg')
-saveFile(photosFolder + "5stawow/" +createFileName('.jpg'), response.read())
+saveFile(psFileName, response.read())
 
 print "Buczynowa dolinka."
 response = urllib2.urlopen('http://kamery.topr.pl/stawy1/stawy1.jpg')
-saveFile(photosFolder + "buczynowa/" +createFileName('.jpg'), response.read())
+saveFile(buFileName, response.read())
 
 print "Gąsienicowa dolina."
 response = urllib2.urlopen('http://kamery.topr.pl/gasienicowa/gasie.jpg')
-saveFile(photosFolder + "gasienicowa/" +createFileName('.jpg'), response.read())
+saveFile(gaFileName, response.read())
 
 print "Goryczkowa dolina."
 response = urllib2.urlopen('http://kamery.topr.pl/goryczkowa/gorycz.jpg')
-saveFile(photosFolder + "goryczkowa/" +createFileName('.jpg'), response.read())
+saveFile(goFileName, response.read())
 
 print "Chochołowska dolina."
 response = urllib2.urlopen('http://kamery.topr.pl/chocholowska/chocholow.jpg')
-saveFile(photosFolder + "chocholowska/" +createFileName('.jpg'), response.read())
+saveFile(chFileName, response.read())
 
 print "Meteogram."
 response = urllib2.urlopen('http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&fdate=2015012806&row=487&col=232&lang=pl')
 saveFile(photosFolder + "meteo/" +createFileName('.png'), response.read())
 
+#Mergowanie plikow
+command =  "convert \( "+m1FileName+" "+m2FileName+" "+psFileName+" +append \) \
+          \( "+buFileName+" "+gaFileName+" "+goFileName+" +append \) \
+          \( "+chFileName+" +append \) \
+          -background none -append "+allFileName 
+print command
+os.system(command)
 

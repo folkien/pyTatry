4d3c3f9890fedf9cff05098965b24309e9941a78
spasz <folkus@gmail.com>
2015-02-03 23:55:38 +0100

    prosty merger plikow jako klasa.

--- pyLogger.py
+++ pyLogger.py
@@ -3,16 +3,14 @@
 import os
 import functions, settings
 
-# Sciaganie obrazu z kamer.
-for kamera in settings.kamery : 
-    print kamera.nazwa
-    functions.saveFile(settings.photosFolder + kamera.folder + settings.actualDate + kamera.rozszerzenie, kamera.fetchData())
+for kraina in settings.swiat: 
+    print kraina.nazwa + "."
 
-#Mergowanie plikow
-#print "Zdjęcie całego dnia."
-#command =  "convert \( "+m1FileName+" "+m2FileName+" "+psFileName+" +append \) \
-#          \( "+buFileName+" "+gaFileName+" "+goFileName+" +append \) \
-#          \( "+chFileName+" +append \) \
-#          -background none -append "+allFileName 
-#os.system(command)
+    # Sciaganie obrazu z kamer.
+    for kamera in kraina.listaKamer : 
+        print kamera.nazwa + "."
+        functions.saveFile(settings.photosFolder + kamera.folder + settings.actualDate + kamera.rozszerzenie, kamera.fetchData())
+
+    kraina.kolazKamer()
+
 
--- settings.py
+++ settings.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import functions, os, urllib2, re
+import functions, os, urllib2, re, math
 
-class cameraLocalization:
+class kameraInternetowa:
 		
-		def __init__(self,n,l,f, ext = ".jpg", k = "tatry"):
+		def __init__(self,n,l,f, ext = ".jpg"):
 				self.nazwa 	= n
 				self.link  	= l
 				self.folder = f
 				self.rozszerzenie = ext
-				self.kraina = k
 
 		def fetchData(self):
 				response = urllib2.urlopen(self.link)
 				return response.read()
 
-class stopienZagrozenia(cameraLocalization):
+class stopienZagrozenia(kameraInternetowa):
 		
 		def fetchData(self):
 				#odczytujemy stronę www z zagrożeniem lawinowym
@@ -24,6 +23,41 @@ class stopienZagrozenia(cameraLocalization):
 				#sciagamy obrazek ze strony TOPR'u
 				response = urllib2.urlopen("http://www.topr.pl/" + stopien)
 				return response.read()
+
+class krainaGeograficzna:
+		
+		def __init__(self,n="beznazwy",f="./",k=[], ext = ".jpg"):
+				self.nazwa 			= n
+				self.folder 		= f
+				self.listaKamer		= k
+				self.rozszerzenie 	= ext
+
+		def kolazKamer(self):
+				print "Zdjęcie całego dnia."
+				#Mergowanie plikow
+				command 	= "convert "
+				photosNumber= len(self.listaKamer)
+				sizex 		= math.ceil( math.sqrt( photosNumber ) ) 
+				n 			= 0
+				row 		= ""
+				while (n < photosNumber):
+						row += photosFolder + self.listaKamer[n].folder + actualDate + self.listaKamer[n].rozszerzenie + " "
+						n += 1
+						# Jezeli trzeba kończyć rząd to kończymy
+						if (n%sizex == 0):
+								command += "\( " + row + " +append \)  "
+								row 	 = ""
+				command += "-background none -append "+ photosFolder + self.folder + actualDate + self.rozszerzenie 
+				print command
+				os.system(command)
+						
+
+
+				#command =  "convert \( "+m1FileName+" "+m2FileName+" "+psFileName+" +append \) \
+				#		  \( "+buFileName+" "+gaFileName+" "+goFileName+" +append \) \
+				#		  \( "+chFileName+" +append \) \
+				#		  -background none -append "+allFileName 
+				#os.system(command)
 
 
 # ścieżki i foldery w programie				
@@ -34,15 +68,24 @@ photosFolder = scriptFolder + "/data/photos/"
 actualDate = functions.createFileName('')
 allFileName= photosFolder + "tatry/" + actualDate
 
-kamery = []
-kamery.append( cameraLocalization("Morskie Oko 1", "http://kamery.topr.pl/moko/moko_01.jpg", 				"moko1/" ) )
-kamery.append( cameraLocalization("Morskie Oko 2", "http://kamery.topr.pl/moko_TPN/moko_02.jpg", 			"moko2/" ) )
-kamery.append( cameraLocalization("Dol 5 stawów", "http://kamery.topr.pl/stawy2/stawy2.jpg", 				"5stawow/" ) )
-kamery.append( cameraLocalization("Buczynowa dolinka", "http://kamery.topr.pl/stawy1/stawy1.jpg", 			"buczynowa/" ) )
-kamery.append( cameraLocalization("Hala gąsienicowa", "http://kamery.topr.pl/gasienicowa/gasie.jpg", 		"gasienicowa/" ) )
-kamery.append( cameraLocalization("Goryczkowa", "http://kamery.topr.pl/goryczkowa/gorycz.jpg", 				"goryczkowa/" ) )
-kamery.append( cameraLocalization("Dol Chochołowska", "http://kamery.topr.pl/chocholowska/chocholow.jpg", 	"chocholowska/" ) )
-kamery.append( cameraLocalization("Meteogram", "http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=487&col=232&lang=pl", "meteo/", ".png" ) )
-kamery.append( stopienZagrozenia( "Stopień zagrożenia lawinowego", "http://www.topr.pl/wwt/warunki-w-tatrach-2", 	"lawiny/" ) )
+
+# kamery w Tatrach
+kameryTatry = []
+kameryTatry.append( kameraInternetowa("Morskie Oko 1", "http://kamery.topr.pl/moko/moko_01.jpg", 				"moko1/" ) )
+kameryTatry.append( kameraInternetowa("Morskie Oko 2", "http://kamery.topr.pl/moko_TPN/moko_02.jpg", 			"moko2/" ) )
+kameryTatry.append( kameraInternetowa("Dol 5 stawów", "http://kamery.topr.pl/stawy2/stawy2.jpg", 				"5stawow/" ) )
+kameryTatry.append( kameraInternetowa("Buczynowa dolinka", "http://kamery.topr.pl/stawy1/stawy1.jpg", 			"buczynowa/" ) )
+kameryTatry.append( kameraInternetowa("Hala gąsienicowa", "http://kamery.topr.pl/gasienicowa/gasie.jpg", 		"gasienicowa/" ) )
+kameryTatry.append( kameraInternetowa("Goryczkowa", "http://kamery.topr.pl/goryczkowa/gorycz.jpg", 				"goryczkowa/" ) )
+kameryTatry.append( kameraInternetowa("Dol Chochołowska", "http://kamery.topr.pl/chocholowska/chocholow.jpg", 	"chocholowska/" ) )
+kameryTatry.append( kameraInternetowa("Meteogram", "http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=487&col=232&lang=pl", "meteo/", ".png" ) )
+kameryTatry.append( stopienZagrozenia( "Stopień zagrożenia lawinowego", "http://www.topr.pl/wwt/warunki-w-tatrach-2", 	"lawiny/" ) )
+
+# tworzymy krainę tatr
+tatry = krainaGeograficzna("Tatry","tatry/",kameryTatry)
+
+# tworzymy swiat
+swiat = []
+swiat.append(tatry)
 
 
